04fed058a07024e956ccdda4125db3db47f0a5bd
vrbr <lpvamaria@gmail.com>
2021-12-27 20:05:22 +0200

    Скрипты в одном файле, настроен адаптив, отключены слайдеры на мобиле

--- js/channel-first.js
+++
@@ -1,9 +0,0 @@
-const swiperFirst = new Swiper('.channel-first__slider', {
-    loop: true,
-    slidesPerView: "auto",
-    spaceBetween: 40,
-    navigation: {
-        nextEl: '.channel-first-button-next',
-        prevEl: '.channel-first-button-prev',
-    },
-});
--- js/channel-second.js
+++
@@ -1,9 +0,0 @@
-const swiperSecond = new Swiper('.channel-second__slider', {
-    loop: true,
-    slidesPerView: "auto",
-    spaceBetween: 40,
-    navigation: {
-        nextEl: '.channel-second-button-next',
-        prevEl: '.channel-second-button-prev',
-    },
-});
